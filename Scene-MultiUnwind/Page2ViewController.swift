39b9be254c5f0f86c173e319a37bdac4551bcf68
한규철 <hkc@hangyucheol-ui-MacBookPro.local>
2022-04-20 16:05:22 -0700

    first commit

---
+++ Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 한규철 on 3/21/R4.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue){
+        
+    }
+}
---
+++ Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 한규철 on 3/21/R4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
